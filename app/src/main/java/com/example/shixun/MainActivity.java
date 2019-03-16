4e48a30c5d9405f5c26aceecc2e4d79df0a80ef3
WJ <397272087@qq.com>
2019-03-16 15:29:58 +0800

    ShiXun项目

--- app/src/main/java/com/example/shixun/MainActivity.java
+++ app/src/main/java/com/example/shixun/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.shixun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.shixun.utils.LogUtils;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        LogUtils.d("123");
+        Log.d("hhh", "hello123456");
+
+    }
+
+    private void initView() {
     }
 }
+
